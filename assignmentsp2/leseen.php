5c447153114a96b26cb48b31a3ddc14950592d64
brian74tech <bri_sg@hotmail.com>
2023-04-15 13:26:37 +0000

    y

--- assignmentsp2/leseen.php
+++ assignmentsp2/leseen.php
@@ -50,7 +50,7 @@
                 ?>
 
         </p><br>
-        <h5>Sample 2: while loop</h5><br>
+        <h5>Sample 2: while loop</h5>
         <pre>
     &lt;?php
         $i = 1; 
@@ -71,7 +71,7 @@
             ?>
 
         </p><br>
-        <h5>Sample 3: do while </h5><br>
+        <h5>Sample 3: do while </h5>
         <pre>
     &lt;?php
         $i = 0; 
