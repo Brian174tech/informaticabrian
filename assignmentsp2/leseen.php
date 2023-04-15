4eeceba5c2d2fb539088a086cb2c31f89546b0c0
brian74tech <bri_sg@hotmail.com>
2023-04-15 12:44:35 +0000

    added css

--- assignmentsp2/leseen.php
+++ assignmentsp2/leseen.php
@@ -5,8 +5,8 @@
   <meta name="keywords" content="Brian is a Software engineer in the making and this is his personal website" />
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <link href="css/mainstyle.css" type="text/css" rel="stylesheet" />
-  <link href="css/color.css" type="text/css" rel="stylesheet" />
+  <link href="../css/mainstyle.css" type="text/css" rel="stylesheet" />
+  <link href="../css/color.css" type="text/css" rel="stylesheet" />
 </head>
 <body>
     <header class="logomain">
