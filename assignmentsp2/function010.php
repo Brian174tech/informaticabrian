153155759792686f889e1e2cdbe379f0f7ff7f02
brian74tech <bri_sg@hotmail.com>
2023-04-23 17:09:11 +0000

    y

--- assignmentsp2/function010.php
+++ assignmentsp2/function010.php
@@ -8,7 +8,11 @@ if(is_numeric($_POST['nummer'])){
         $opties = array("1"=>"Zeer Slecht","2"=>"Slecht","3"=>"Matig","4"=>"ruim onvoldoende","5"=>"onvoldoende","6"=>"voldoende","7"=>"ruim voldoende","8"=>"Goed", "9"=>"Zeer goed", "10"=>"uitmuntend");
         echo "<p>De cijferwaarde van nummer $nummer = $opties[$nummer]</p>";
         echo "<p><a href=\"javascript:history.back()\"> Pagina terug!</a></p>\n";
-    } else{}
+    } else{
+        echo("<p>nummer is geen (geldig) numeriek</p>");
+        echo "<p><a href=\"javascript:history.back()\"> Pagina terug!</a></p>\n";
+        exit();
+    }
 } if(is_numeric($_POST['nummercount'])){
     $nummer2 = $_POST['nummercount'];
     $nummer = (int)$nummer2;
@@ -16,7 +20,11 @@ if(is_numeric($_POST['nummer'])){
         $resultaat = strlen((string)$nummer);
         echo "<p>De lengte van je nummer $nummer = $resultaat</p>";
         echo "<p><a href=\"javascript:history.back()\"> Pagina terug!</a></p>\n";
-    }else{}
+    }else{
+        echo("<p>nummer is geen (geldig) numeriek</p>");
+        echo "<p><a href=\"javascript:history.back()\"> Pagina terug!</a></p>\n";
+        exit();
+    }
 } else{
     echo("<p>nummer is geen (geldig) numeriek</p>");
     echo "<p><a href=\"javascript:history.back()\"> Pagina terug!</a></p>\n";
