3e762aa6ebf332b51643fd0aaf08b532c4b583c7
brian74tech <bri_sg@hotmail.com>
2023-04-23 16:27:21 +0000

    y

--- assignmentsp2/function010.php
+++ assignmentsp2/function010.php
@@ -2,7 +2,7 @@
 require_once '../default/header.php';
 
 if(is_numeric($_POST['nummer'])){
-    $nummer = $_POST;
+    $nummer = $_POST['nummer'];
     if ($nummer > 1 && $nummer < 10){
         $opties = array("1"=>"Zeer Slecht","2"=>"Slecht","3"=>"Matig","4"=>"ruim onvoldoende","5"=>"onvoldoende","6"=>"voldoende","7"=>"ruim voldoende","8"=>"Goed", "9"=>"Zeer goed", "10"=>"uitmuntend");
         echo($opties);
