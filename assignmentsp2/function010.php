84f6f7c73b5099d5c627be251902cc6ae43c7d51
brian74tech <bri_sg@hotmail.com>
2023-04-23 16:29:12 +0000

    y

--- assignmentsp2/function010.php
+++ assignmentsp2/function010.php
@@ -5,10 +5,8 @@ if(is_numeric($_POST['nummer'])){
     $nummer = $_POST['nummer'];
     if ($nummer > 1 && $nummer < 10){
         $opties = array("1"=>"Zeer Slecht","2"=>"Slecht","3"=>"Matig","4"=>"ruim onvoldoende","5"=>"onvoldoende","6"=>"voldoende","7"=>"ruim voldoende","8"=>"Goed", "9"=>"Zeer goed", "10"=>"uitmuntend");
-        echo($opties);
-        echo($nummer);
-        echo "<p>$opties[$nummer]</p>";
-        
+        echo "<p>De vertaling van waarde $nummer = $opties[$nummer]</p>";
+        echo "<p><a href=\"javascript:history.back()\"> Pagina terug!</a></p>\n";
     } 
 } else{
     echo("<p>nummer is geen numeriek tussen 1 en 10</p>");
