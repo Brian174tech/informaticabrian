23932f1199bf9b781491a82a21e3e09382fe6a3e
brian74tech <bri_sg@hotmail.com>
2023-04-23 14:56:38 +0000

    y

--- assignmentsp2/lestwee.php
+++ assignmentsp2/lestwee.php
@@ -111,8 +111,12 @@
     ?>
     <br>
     <p>
-      SessionID = <?=session_id() ?> <br>
-      Page visit counter: <?=$counter ?> .
+      SessionID = <?= session_id() ?> <br>
+      Page visit counter: <?= $counter ?> .
+    </p>
+    <p>
+      <img id="activitydiagram" src="../images/activity.png" title="activity switch image" />
+      Switch activity diagram
     </p>
   </section>
 </main>
