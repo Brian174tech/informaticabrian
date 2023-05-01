03287a503f970f6db908e9b7b553217a94932845
brian74tech <bri_sg@hotmail.com>
2023-05-01 22:03:37 +0000

    y

--- assignmentsp2/lesdrie.php
+++ assignmentsp2/lesdrie.php
@@ -103,49 +103,42 @@
 
         echo ("<br><h5>Magic methods</h5><p>Magische methoden (of magic methods) in PHP zijn voorgedefinieerde methoden.  Ze beginnen allemaal met de naam __ en eindigen met de naam van de methode, deze worden automatisch aangeroepen/uitgevoerd in bepaalde situaties, zoals wanneer een object wordt gemaakt of als een onbestaande methode wordt aangeroepen. Voorbeelden zijn __construct(bij een create), __destruct(bij een destroy), __toString(bij het behandelen van een object als een string), __get(read), __set(write), __isset(validity), etc. </p><br>");
         echo ("<h5>Polymorphisme</h5><p>Polymorfisme is een concept in de objectgeoriënteerde programmeertaal waarbij een object van verschillende klassen kan worden geïnstantieerd en op verschillende manieren kan worden gebruikt, afhankelijk van de context waarin het wordt gebruikt. Het betekent dat dezelfde methode verschillende implementaties heeft in verschillende klassen en dat de methode kan worden aangeroepen op objecten van die verschillende klassen zonder dat de methode opnieuw moet worden geschreven. Dit kan de efficiëntie van het code schrijven en onderhouden verbeteren, omdat het betekent dat bepaalde stukken code kunnen worden hergebruikt in verschillende contexten. </p><br>");
-        echo ("<h5>Dependency injection container</h5><p>Een dependency injection container is een object dat wordt gebruikt om afhankelijkheden in een programma te beheren en te leveren aan objecten die deze nodig hebben. Het maakt het mogelijk om objecten te maken zonder dat ze afhankelijk zijn van andere objecten, en om afhankelijkheden te wijzigen zonder de code van het object te wijzigen.</p><br>");
+        echo ("<h5>Dependency injection container</h5><p>Een dependency injection container is een object dat wordt gebruikt om afhankelijkheden in een programma te beheren en te leveren aan objecten die deze nodig hebben. Het maakt het mogelijk om objecten te maken zonder dat ze afhankelijk zijn van andere objecten, en om afhankelijkheden te wijzigen zonder de code van het object te wijzigen. In plaats van handmatig elke afhankelijkheid te beheren, kan de container dit automatisch doen op basis van de configuratie die is opgegeven bij het opstarten van de applicatie. Dit maakt het eenvoudiger om de applicatie te schalen, te testen en te onderhouden, omdat elke component duidelijk is gedefinieerd en gemakkelijk kan worden vervangen of geüpgraded zonder dat dit gevolgen heeft voor de rest van de applicatie. </p><br>");
+        echo ('<h5>Generator</h5><p>Een generator in PHP is een speciaal type functie waarmee je gegevens kunt genereren en terugsturen naar een loop zonder de geheugenbelasting van het opslaan van de hele lijst met gegevens. In plaats daarvan wordt de generator voortdurend nieuwe gegevens gegenereerd en teruggestuurd totdat het einde is bereikt. Voorbeeld 1. traditionele for each loop vs 2. generator</p><pre>
+        1. for each
+        $array = range(1, 100000);
 
+        function processArray($arr) {
+            foreach ($arr as $element) {
+            }
+        }
+        
+        processArray($array);
+
+        Memory usage before: 4 MB
+        Memory usage after: 34 MB
+
+        2. generator
+        function processArray($arr) {
+            foreach ($arr as $element) {
+                yield $element;
+            }
+        }
+        
+        $array = range(1, 100000);
+        
+        foreach (processArray($array) as $element) {
+        }
+
+        Memory usage before: 4 MB
+        Memory usage after: 4 MB
+        </pre>
+        <p>Het verschil komt doordat de generator slechts één element van de array in het werkgeheugen laadt en bewerkt voordat het doorgegeven wordt aan de loop. De for each loop laadt alle data in en gaat ze een voor een langs tot ze klaar is.</p>');
+        echo ("<br><h5>Inheritance en Composition</h5><p>Inheritance is een manier waarbij een klasse (de 'child class') de eigenschappen en methoden van een andere klasse (de 'parent class') erft. Composition is een manier waarbij een klasse bestaat uit verschillende objecten van andere klassen, die samenwerken om de functionaliteit van de klasse te bieden. Inheritance gaat bijvoorbeeld door extends, superklasse met overerving naar child - handig bij veel hebruikbare code. Composition kan met onafhankelijke classen die elkaar aanroepen, waar geen duidelijke hierarchie is tussen de klassen. </p><br>");
+        echo ("<h5>Traits</h5><p>Traits zijn een manier om herbruikbare stukjes code te maken die kunnen worden toegepast op meerdere klassen zonder de noodzaak van herhaling van dezelfde code. Traits zijn dus meer gericht op hergebruik van code dan op de structurering van de relaties tussen klassen zoals composition of inheritance. Een trait is eigenlijk een herbruikbare set van methoden die je kunt toevoegen aan een klasse. Het belangrijkste verschil tussen een trait en een klasse is dat een trait zelf geen instantie kan hebben en dus geen object kan worden gemaakt. In plaats daarvan moet een trait worden gebruikt in combinatie met een klasse die de functionaliteit van de trait implementeert.</p><br>");
+        echo ("<h5></h5><p></p><br>");
         ?>
         <br>
-
-        <?php
-        define('DB_HOST', 'localhost');
-        define('DB_NAME', 'schoolopdracht');
-        define('DB_USER', 'bri_sg');
-        define('DB_PASS', '28172817');
-
-        function connectDatabase()
-        {
-            $conn = 'mysql:host=' . DB_HOST . ';DB_NAME=' . DB_NAME;
-
-            try {
-                return new PDO($conn, DB_USER, DB_PASS);
-            } catch (PDOException $e) {
-                return NULL;
-            }
-        }
-
-        $db = connectDatabase();
-        if (is_null($db))
-            die('<h1>Database-verbinding mislukt</h1>');
-        echo 'verbinding: gelukt';
-
-        $sql = 'SELECT * FROM schoolopdracht.gebruikers';
-
-        $statement = $db->prepare($sql);
-        $statement->execute();
-        $result = $statement->fetchall(PDO::FETCH_OBJ);
-
-        foreach ($result as $record) {
-            echo '<ul>';
-
-            foreach ($record  as $key => $value) {
-                echo "<li>$key => $value</li>";
-            }
-            echo '</ul>';
-        }
-
-        ?>
     </section>
 </main>
 <?php require_once '../default/footer.html'; ?>
