ffdb37248ea0167787a9606941a133bfb571fc79
Brian174tech <114503613+Brian174tech@users.noreply.github.com>
2023-07-09 21:20:43 +0200

    added super great buddy page

---
+++ brian/assessment/objectmodel.php
@@ -0,0 +1,47 @@
+<?php
+
+        
+
+    class ObjectO{
+        public $objectId;
+        public $objectColor;
+        public $objectName;
+        
+        
+        function setObjectName($objectName){
+            $this->objectName = $objectName;
+        }
+        function getObjectName()
+        {
+            return $this->objectName;
+        }
+
+        function setObjectId($objectId){
+            $this->objectId = $objectId;
+        }
+        function getObjectId()
+        {
+            return $this->objectId;
+        }
+
+        function setObjectColor($objectColor){
+            $this->objectColor = $objectColor;
+        }
+        function getObjectColor()
+        {
+            return $this->objectColor;
+        }
+
+    }
+
+    class ObjectList{
+        public $objectList;
+
+        function setObjectList($objectList){
+            $this->objectList = $objectList;
+        }
+        function getObjectList()
+        {
+            return $this->objectList;
+        }
+    }
